4b4e5f954fb56a6ba097f7994400dbeb4dc5b325
nasimbayati <nasim.bayati1993@gmail.com>
2026-01-31 20:36:36 -0600

    Project #1 - Dockerized Java Monster App

---
+++ my-docker-java-monster/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Monster myMonster = new Monster("Emberclaw", "Fire");
+
+        System.out.println("Your monster has been created.");
+        System.out.println("Description: " + myMonster.getDescription());
+    }
+}
