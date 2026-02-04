52dcc5d61dd7c134a5b8120b4a2892a056e71151
nasimbayati <nasim.bayati1993@gmail.com>
2026-02-03 19:30:28 -0600

    Added the print statement Your monster has been created at the end of the constructor, introduce description variable to save the statement for description, then used a method named getDescription to return it, it's more polished this way

--- my-docker-java-monster/Monster.java
+++ my-docker-java-monster/Monster.java
@@ -2,13 +2,16 @@ public class Monster {
 
     private String name;
     private String type;
+	private String description;
 
     public Monster(String name, String type) {
         this.name = name;
         this.type = type;
+        this.description = "The monster, \"" + name + "\", with the type \"" + type + "\" is from the Neon Ark training program.";
+        System.out.println("Your monster has been created.");
     }
 
     public String getDescription() {
-        return name + " is a " + type + "-type monster from the Neon Ark training program.";
+        return description;
     }
 }
