0e4d1de425903d02a374b47fc57c89d10bb2df81
nasimbayati <nasim.bayati1993@gmail.com>
2026-02-06 10:36:10 -0600

    Removed the print statement from constructor, and removed description from the constructor and added the statement to the return statement in getDescription

--- my-docker-java-monster/Monster.java
+++ my-docker-java-monster/Monster.java
@@ -2,16 +2,13 @@ public class Monster {
 
     private String name;
     private String type;
-	private String description;
 
     public Monster(String name, String type) {
         this.name = name;
         this.type = type;
-        this.description = "The monster, \"" + name + "\", with the type \"" + type + "\" is from the Neon Ark training program.";
-        System.out.println("Your monster has been created.");
     }
 
     public String getDescription() {
-        return description;
+        return "The monster, \"" + name + "\", with the type \"" + type + "\" is from the Neon Ark training program.";
     }
 }
